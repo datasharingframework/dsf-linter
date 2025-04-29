8941b7192ebe809687a9f25cb4f8649d903c49de
Khalil24 <momalla@stud.hs-heilbronn.de>
2025-04-29 17:29:47 +0200

    - ValidationType updated. - ValidationType of the class FhirFileDateNoPlaceholderValidationItem is corrected

--- validator-api/src/main/java/dev/dsf/utils/validator/item/FhirFileDateNoPlaceholderValidationItem.java
+++ validator-api/src/main/java/dev/dsf/utils/validator/item/FhirFileDateNoPlaceholderValidationItem.java
@@ -6,18 +6,18 @@ import java.io.File;
 
 /**
  * Validation item indicating that the date field in an ActivityDefinition does not contain a placeholder.
- * Corresponds to {@link ValidationType#ACTIVITY_DEFINITION_DATE_NO_PLACEHOLDER}.
+ * Corresponds to {@link ValidationType#FILE_DATE_NO_PLACEHOLDER}.
  */
 public class FhirFileDateNoPlaceholderValidationItem extends FhirElementValidationItem
 {
     public FhirFileDateNoPlaceholderValidationItem(File resourceFile, String fhirReference)
     {
-        super(ValidationSeverity.WARN, resourceFile != null ? resourceFile.getName() : "unknown.xml", fhirReference, ValidationType.ACTIVITY_DEFINITION_DATE_NO_PLACEHOLDER,
+        super(ValidationSeverity.WARN, resourceFile != null ? resourceFile.getName() : "unknown.xml", fhirReference, ValidationType.FILE_DATE_NO_PLACEHOLDER,
                 "ActivityDefinition date does not contain a placeholder");
     }
 
     public FhirFileDateNoPlaceholderValidationItem(File resourceFile, String fhirReference, String description)
     {
-        super(ValidationSeverity.WARN, resourceFile != null ? resourceFile.getName() : "unknown.xml", fhirReference, ValidationType.ACTIVITY_DEFINITION_DATE_NO_PLACEHOLDER, description);
+        super(ValidationSeverity.WARN, resourceFile != null ? resourceFile.getName() : "unknown.xml", fhirReference, ValidationType.FILE_DATE_NO_PLACEHOLDER, description);
     }
 }
