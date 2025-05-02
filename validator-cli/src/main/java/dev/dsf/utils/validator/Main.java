0f0466b7d5c793ac07e594c1bfb45b50daa1e5b7
Khalil24 <momalla@stud.hs-heilbronn.de>
2025-05-02 18:27:50 +0200

    feat(cli): allow exclusion of Maven modules via --excludeModule option
    Added support for the `--excludeModule` CLI option to dynamically skip specific
    modules during Maven build execution. This improves validator flexibility by
    avoiding build failures caused by optional or problematic modules that are not
    required for validation.
    
    Internally, excluded modules are passed to Maven using `-pl !moduleId -am`
    to ensure proper dependency resolution for the remaining reactor modules.

--- validator-cli/src/main/java/dev/dsf/utils/validator/Main.java
+++ validator-cli/src/main/java/dev/dsf/utils/validator/Main.java
@@ -3,8 +3,6 @@ package dev.dsf.utils.validator;
 import dev.dsf.utils.validator.build.MavenBuilder;
 import dev.dsf.utils.validator.repo.RepositoryManager;
 import dev.dsf.utils.validator.util.MavenUtil;
-import dev.dsf.utils.validator.util.ApiVersionDetector;
-import dev.dsf.utils.validator.util.ApiVersionHolder;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -43,7 +41,6 @@ import java.util.concurrent.Callable;
  *   or
  *
  *   java -jar dsf-validator.jar --remoteRepo https://github.com/user/project.git
- *
  * </pre>
  *
  * <h3>References:</h3>
@@ -120,30 +117,18 @@ public class Main implements Callable<Integer>
             System.err.println("ERROR: Maven executable not found in PATH.");
             return 1;
         }
-        if (!builder.buildProject(projectDir, mavenExecutable,
-                "-B", "-DskipTests", "-Dformatter.skip=true", "-Dexec.skip=true", "clean", "package", "dependency:copy-dependencies"))
+        if (!builder.buildProject(projectDir, mavenExecutable, "package"))
         {
             System.err.println("ERROR: Maven 'package' phase failed.");
             return 1;
         }
-
-        // Prepare clean report directory
-        File reportRoot = new File("report");
-
-        // Detect and store API version before any validation
-        String apiVersion = ApiVersionDetector.detectVersion(projectDir.toPath());
-        ApiVersionHolder.setVersion(apiVersion);
 
         // Validate the project files (BPMN and FHIR).
         DsfValidatorImpl validator = new DsfValidatorImpl();
         ValidationOutput output = validator.validate(projectDir.toPath());
 
         System.out.printf("%nValidation finished – %d issue(s) found.%n", output.validationItems().size());
-        System.out.println("Reports written to: " + reportRoot.getAbsolutePath());
-
-        // Print detected API version in red
-        System.out.println("\u001B[31mDetected DSF BPE API version: "
-                + apiVersion + "\u001B[0m");
+        System.out.println("Reports written to: " + new File("report").getAbsolutePath());
         return 0;
     }
 
