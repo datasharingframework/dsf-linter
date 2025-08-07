213e85f65ee865c19e130facc8e10ed4e3c9a821
Khalil24 <momalla@stud.hs-heilbronn.de>
2025-08-07 20:30:16 +0200

    feat: update DsfValidator to include additional exception handling in validate method

--- validator-core/src/main/java/dev/dsf/utils/validator/util/DsfValidator.java
+++ validator-core/src/main/java/dev/dsf/utils/validator/util/DsfValidator.java
@@ -1,5 +1,9 @@
 package dev.dsf.utils.validator.util;
 
+import dev.dsf.utils.validator.exception.MissingServiceRegistrationException;
+import dev.dsf.utils.validator.exception.ResourceValidationException;
+
+import java.io.IOException;
 import java.nio.file.Path;
 
 public interface DsfValidator {
@@ -18,6 +22,9 @@ public interface DsfValidator {
      * the same as their FHIR resource counterparts.
      * @param path Path to a project/directory which should be validated and where the outlined directory structure is found
      * @return A ValidationOutput-Object containing the results of the validation
+     * @throws ResourceValidationException if a resource has a syntax error.
+     * @throws IllegalStateException if a unique ProcessPluginDefinition cannot be found.
+     * @throws IOException if a file system error occurs.
      */
-    ValidationOutput validate(Path path);
+    ValidationOutput validate(Path path) throws ResourceValidationException, IllegalStateException, IOException, InterruptedException, MissingServiceRegistrationException;
 }
