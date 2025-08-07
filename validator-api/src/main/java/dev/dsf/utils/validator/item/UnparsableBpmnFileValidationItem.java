9a49717f768b10754d69119d017b6c22bd1ab6fc
Khalil24 <momalla@stud.hs-heilbronn.de>
2025-08-07 20:27:30 +0200

    - deleted this item because the validator breaks in this case.

--- validator-api/src/main/java/dev/dsf/utils/validator/item/UnparsableBpmnFileValidationItem.java
+++
@@ -1,9 +0,0 @@
-package dev.dsf.utils.validator.item;
-
-import dev.dsf.utils.validator.ValidationSeverity;
-
-public class UnparsableBpmnFileValidationItem extends AbstractValidationItem {
-    public UnparsableBpmnFileValidationItem(ValidationSeverity severity) {
-        super(severity);
-    }
-}
