dff01f606dd775ce0556003c4058a501dee135fc
Khalil24 <momalla@stud.hs-heilbronn.de>
2025-09-26 20:27:44 +0200

    Cleanup: Delete obsolete validation item classes
    Removed `validationItemsKlassen` which are no longer necessary or have been
    replaced by a new approach.

--- validator-api/src/main/java/dev/dsf/utils/validator/item/PluginDefinitionInvalidValidationItem.java
+++
@@ -1,9 +0,0 @@
-package dev.dsf.utils.validator.item;
-
-import dev.dsf.utils.validator.ValidationSeverity;
-
-public class PluginDefinitionInvalidValidationItem extends PluginDefinitionValidationItem {
-    public PluginDefinitionInvalidValidationItem(ValidationSeverity severity) {
-        super(severity);
-    }
-}
