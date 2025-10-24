50bca4202fd45f16766ddee60f96de29bb8c81fe
Khalil24 <momalla@stud.hs-heilbronn.de>
2025-10-24 17:33:12 +0200

    Add InputType enum for unified input handling
    - Define supported input types: LOCAL_DIRECTORY, GIT_REPOSITORY, LOCAL_JAR_FILE, REMOTE_JAR_URL
    - Provide comprehensive documentation for each type
    - Foundation for unified input resolution

---
+++ validator-core/src/main/java/dev/dsf/linter/input/InputType.java
@@ -0,0 +1,59 @@
+package dev.dsf.linter.input;
+
+/**
+ * Enumeration of supported input types for the DSF validator.
+ * <p>
+ * This enum defines the different types of input sources that can be
+ * validated by the DSF validator, enabling unified handling of various
+ * project formats and locations.
+ * </p>
+ *
+ * <h3>Supported Input Types:</h3>
+ * <ul>
+ *   <li><b>LOCAL_DIRECTORY</b> - Standard Maven/Gradle project directory on local filesystem</li>
+ *   <li><b>GIT_REPOSITORY</b> - Remote Git repository (HTTP/HTTPS/SSH)</li>
+ *   <li><b>LOCAL_JAR_FILE</b> - Compiled JAR file on local filesystem</li>
+ *   <li><b>REMOTE_JAR_URL</b> - JAR file accessible via HTTP/HTTPS</li>
+ * </ul>
+ *
+ * @author DSF Development Team
+ * @since 1.0.0
+ */
+public enum InputType {
+
+    /**
+     * Local directory containing a Maven or Gradle project.
+     * <p>
+     * Expected structure: pom.xml or build.gradle in root,
+     * with standard src/main/resources layout.
+     * </p>
+     */
+    LOCAL_DIRECTORY,
+
+    /**
+     * Remote Git repository URL.
+     * <p>
+     * Supports protocols: http://, https://, git://, ssh://, git@
+     * Repository will be cloned to temporary directory before validation.
+     * </p>
+     */
+    GIT_REPOSITORY,
+
+    /**
+     * Local JAR file containing a compiled DSF plugin.
+     * <p>
+     * Expected structure: Standard JAR with META-INF/services registration,
+     * BPMN files in bpe/, FHIR resources in fhir/.
+     * </p>
+     */
+    LOCAL_JAR_FILE,
+
+    /**
+     * Remote JAR file accessible via HTTP/HTTPS.
+     * <p>
+     * JAR will be downloaded to temporary directory before extraction.
+     * Supports standard HTTP/HTTPS URLs including Maven repository URLs.
+     * </p>
+     */
+    REMOTE_JAR_URL
+}
